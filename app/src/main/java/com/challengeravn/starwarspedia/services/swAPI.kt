c29cb5a67659c36fefc983759972a3b96d8af29a
Roger Rodriguez <rgr88rod@gmail.com>
2023-03-25 10:44:13 -0600

    Added GraphQL Client and manifest permissions

---
+++ app/src/main/java/com/challengeravn/starwarspedia/services/swAPI.kt
@@ -0,0 +1,15 @@
+package com.challengeravn.starwarspedia.services
+
+import android.os.Looper
+import com.apollographql.apollo3.ApolloClient
+
+class swAPI {
+    fun getApolloClient():ApolloClient{
+        //Will work only in main Thread
+        check(Looper.myLooper() ==Looper.getMainLooper()){
+            "Apollo Client can be used onmy in Main Thread"
+        }
+        return ApolloClient.Builder()
+            .serverUrl("https://swapi-graphql.netlify.app/.netlify/functions/index").build()
+    }
+}
